a26918d0fd4c04ab54baa549a5f9e9e4df3ad03f
Fanenosk <50335284+Fanenosk@users.noreply.github.com>
2022-04-20 21:29:41 +0300

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-t = {'id': 'string', 'name': 'string', 'price': 'int', 'sdate': 'timestamp', 'edate': 'timestamp', 'is_actual': 'int'}
-t1 = {'id': 'string', 'name': 'string', 'price': 'int', 'sdate': 'timestamp', 'edate': 'timestamp', 'is_actual': 'int'}
-
-
-print(list(map(lambda key, item: key + ' ' + item, t, t.values())))
-
